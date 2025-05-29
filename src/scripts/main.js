4378a7619dec124aa0ad60b94136377d231f3acf
Oleksandr Polishchuk <crazy8diamond@gmail.com>
2025-05-29 12:53:27 +0300

    add to Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,17 @@
 'use strict';
 
 const list = document.querySelector('ul');
-const items = Array.from(list.children);
 
 function parseSalary(salaryStr) {
   const noDollar = salaryStr.split('$').join('');
   const noComma = noDollar.split(',').join('');
 
-  return parseInt(noComma);
+  return +noComma;
 }
 
-// eslint-disable-next-line no-shadow
 function sortList(list) {
+  const items = Array.from(list.children);
+
   items.sort((a, b) => {
     const salaryA = parseSalary(a.dataset.salary);
     const salaryB = parseSalary(b.dataset.salary);
@@ -22,16 +22,17 @@ function sortList(list) {
   items.forEach((item) => list.appendChild(item));
 }
 
-function getEmployees(_list) {
+function getEmployees(list) {
+  const items = Array.from(list.children);
+
   return items.map((item) => ({
     name: item.textContent.trim(),
     position: item.dataset.position,
     salary: parseSalary(item.dataset.salary),
-    age: item.dataset.age,
+    age: parseInt(item.dataset.age),
   }));
 }
 
 sortList(list);
 
-// eslint-disable-next-line no-unused-vars
 const employees = getEmployees(list);
